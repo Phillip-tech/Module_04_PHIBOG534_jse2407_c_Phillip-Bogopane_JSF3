bdb73b423aaf9bf1dfcd9afdc7f188e00c0dbbbc
phillip-tech <pm.bogopane@gmail.com>
2024-08-06 09:27:16 +0200

    "Updated package dependencies, removed HelloWorld.vue component, and modified Header.vue and style.css files."

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
       <nav class="bg-gray-500 border-gray-200">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="#" class="flex items-center space-x-3">
-            <img src="online-shop.png" class="h-8" alt="SwiftCart Logo" />
+            <img src="assets/online-shop.png" class="h-8" alt="SwiftCart Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">SwiftCart</span>
           </a>
           <button
