e984d28f3dab98694221861c17b676ff57c23f01
phillip-tech <pm.bogopane@gmail.com>
2024-08-06 06:55:12 +0200

    Added Home.vue with template and script importing ProductList component

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h1 class="text-3xl font-bold mb-6">Our Products</h1>
+      <ProductList />
+    </div>
+  </template>
+  
+  <script setup>
+  import ProductList from '../components/ProductList.vue';
+  </script>
+  
