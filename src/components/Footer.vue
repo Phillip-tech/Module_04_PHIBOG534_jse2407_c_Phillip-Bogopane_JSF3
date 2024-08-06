70dfcbe3413ee79d453901319a0ff1100e986d6e
phillip-tech <pm.bogopane@gmail.com>
2024-08-06 06:49:49 +0200

    Added Footer.vue component with basic HTML structure and copyright text

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="bg-gray-500 text-white p-4">
+      <div class="container mx-auto text-center">
+        &copy; 2024 SwiftCart. All rights reserved.
+      </div>
+    </footer>
+  </template>
+  
