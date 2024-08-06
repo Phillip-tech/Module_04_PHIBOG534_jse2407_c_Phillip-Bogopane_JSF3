878b8bf425e9013798b66e4d6ea4b904b23fafa8
phillip-tech <pm.bogopane@gmail.com>
2024-08-07 01:56:26 +0200

    "Updated site.webmanifest with additional icon sizes and modified Header.vue component with changes to cart icon and layout."

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -33,12 +33,24 @@
             <li>
               <a href="#" class="block py-2 px-3 text-white rounded hover:bg-gray-100">Wishlist</a>
             </li>
-            <li class="relative">
-              <a href="#" class="block py-2 px-3 text-white rounded hover:bg-gray-100">Cart</a>
-              <span
-                class="absolute top-0 left-3 -mt-2 -ml-2 flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white"
-                >2</span
+            <li class="hidden lg:block md:block relative">
+              <div class="absolute left-3 -top-4">
+                <p class="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">2</p>
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="h-6 w-6 text-white cursor-pointer" 
               >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
             </li>
             <li>
               <a href="#" class="block py-2 px-3 text-white rounded hover:bg-gray-100">Login</a>
